c6e2b907fa7861b89ab543ba95ea5bab197195e6
yuya-ito-ganbaru <yuya.ito1990@gmail.com>
2024-05-02 22:28:31 +0900

    編集機能

--- src/main/java/com/uhablog/controller/BookController.java
+++ src/main/java/com/uhablog/controller/BookController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -71,12 +73,20 @@ public class BookController {
 	 * @return
 	 */
 	@PostMapping("/book-create")
-	public String saveBook(@ModelAttribute BookForm bookForm, Model model) {
+	public String saveBook(@ModelAttribute @Validated BookForm bookForm, BindingResult result , Model model) {
+		//BindingResultを受け取って、バリデーション結果を受け取る！
 		/*メソッドの説明
 		 * 「http://localhost:8080/book-create」にPostメソッドでリクエストがあったときのメソッド
 		 * 画面で入力された情報をデータベースに登録する。
 		 * 登録完了後は、一覧画面にリダイレクトする。
 		 */
+		
+		//バリデーションエラーの場合
+		if(result.hasErrors()) {
+			//新規登録画面に遷移
+			return "add";
+		}
+		
 		//本を登録する
 		service.insert(bookForm);
 		
